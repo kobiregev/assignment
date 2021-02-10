d1e5a5caf03a183bd39ec42fbada6e97255c494e
kobiregev <63067846+kobiregev@users.noreply.github.com>
2021-02-10 11:08:44 +0200

    Add files via upload

--- server/db.js
+++ server/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost/homeassigment'
+const url = 'mongodb+srv://root:zxc123@cluster0.nxjzc.mongodb.net/homeassigment'
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
 const db = mongoose.connection
 db.on('error', error => console.error('Connection error:', error))
 db.once('open', () => {
-    console.log('connected to database on mongodb.')
+    console.log('connected to database on mongo atalas.')
 })
